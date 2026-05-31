d73997208d6265ddc6496f3c9b919b38f714e1e0
erickh826 <523202523ck@gmail.com>
2026-05-31 23:11:31 +0800

    Add persona package exports

---
+++ src/persona/__init__.py
@@ -0,0 +1,15 @@
+from .engine import PersonaEngine
+from .fixtures import (
+    PERSONA_BUSY_MOM,
+    PERSONA_SENIOR_SHOPPER,
+    PERSONA_TECH_MILLENNIAL,
+)
+from .models import PersonaProfile
+
+__all__ = [
+    "PersonaEngine",
+    "PersonaProfile",
+    "PERSONA_BUSY_MOM",
+    "PERSONA_TECH_MILLENNIAL",
+    "PERSONA_SENIOR_SHOPPER",
+]
